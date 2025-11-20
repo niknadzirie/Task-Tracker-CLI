10d1e737cfbce6baa85836ed4e67512ca495f5fd
Nik Nadzirie Nik Kamaruzamil <nik.nadzirie@stratusauto.com>
2025-11-20 18:16:08 -0500

    First Commit

---
+++ Task Tracker CLI/Program.cs
@@ -0,0 +1,184 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
+using Microsoft.VisualBasic;
+
+namespace Task_Tracker_CLI;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //JSON File
+        List<TaskCli> taskClis = [];
+        string filePath = "data.json";
+
+        if (File.Exists(filePath))
+        {
+            var jsonData = File.ReadAllText(filePath);
+            taskClis = JsonSerializer.Deserialize<List<TaskCli>>(jsonData, new JsonSerializerOptions { WriteIndented = true, Converters = { new JsonStringEnumConverter() } });
+        }
+        else
+        {
+            File.WriteAllText(filePath, "[]");
+        }
+
+        //Write Data
+        void WriteData(List<TaskCli> data)
+        {
+            string jsonString = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true, Converters = { new JsonStringEnumConverter() } });
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        //Add task
+        void AddNewData(string description, TaskStatusEnum status = TaskStatusEnum.Todo)
+        {
+            var newItem = new TaskCli { Id = Guid.NewGuid(), Description = description, Status = status, CreatedAt = DateTimeOffset.Now, UpdatedAt = DateTimeOffset.Now };
+            taskClis.Add(newItem);
+            WriteData(taskClis);
+            Console.WriteLine($"Task Added Successfully (ID: {newItem.Id})");
+        }
+
+        //Update task
+        void UpdateData(Guid targetedId, string description, TaskStatusEnum status = TaskStatusEnum.Todo)
+        {
+            var itemToUpdate = taskClis.FirstOrDefault(c => c.Id == targetedId);
+            if (itemToUpdate != null)
+            {
+                itemToUpdate.Description = description;
+                itemToUpdate.Status = status;
+                itemToUpdate.UpdatedAt = DateTimeOffset.Now;
+            }
+            else
+            {
+                Console.WriteLine("Data not found!");
+            }
+
+            WriteData(taskClis);
+        }
+
+        //Update Status
+        void UpdateStatus(Guid targetedId, TaskStatusEnum status)
+        {
+            var itemToUpdate = taskClis.FirstOrDefault(c => c.Id == targetedId);
+            if (itemToUpdate != null)
+            {
+                itemToUpdate.Status = status;
+                itemToUpdate.UpdatedAt = DateTimeOffset.Now;
+            }
+            else
+            {
+                Console.WriteLine($"Data Not Found!");
+            }
+            WriteData(taskClis);
+        }
+
+        //Delete Task
+        void DeleteData(Guid targetId)
+        {
+            var itemToRemove = taskClis.FirstOrDefault(c => c.Id == targetId);
+            if (itemToRemove != null)
+            {
+                taskClis.Remove(itemToRemove);
+                Console.WriteLine($"Data with ID: {itemToRemove.Id} removed");
+            }
+            else
+            {
+                Console.WriteLine("Data not found!");
+            }
+            WriteData(taskClis);
+        }
+
+        //List all task
+        void ListAllTask()
+        {
+            foreach (var task in taskClis)
+            {
+                Console.WriteLine($"\nID: {task.Id}\nTask: {task.Description}\nStatus: {task.Status}\nCreated At: {task.CreatedAt}\nUpdated At: {task.UpdatedAt}\n\n");
+            }
+        }
+
+        void ListTaskBasedOnStatus(string[] args)
+        {
+            string statusArg = args[1].Replace("-", "");
+            if (Enum.TryParse<TaskStatusEnum>(statusArg, true, out var status))
+            {
+                var filteredTasks = taskClis.Where(c => c.Status == status).ToList();
+                if (filteredTasks.Any())
+                {
+                    foreach (var task in filteredTasks)
+                    {
+                        Console.WriteLine($"\nID: {task.Id}\nTask: {task.Description}\nStatus: {task.Status}\nCreated At: {task.CreatedAt}\nUpdated At: {task.UpdatedAt}\n\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"No task found with status: {status}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid status. Use: Todo, Inprogress, Done");
+            }
+        }
+
+        switch (args[0].ToLower())
+        {
+            case "add":
+                AddNewData(args[1]);
+                break;
+
+            case "list":
+                if (args.Length > 1)
+                {
+                    ListTaskBasedOnStatus(args);
+                    return;
+                }
+                else
+                {
+                    ListAllTask();
+                }
+                break;
+
+            case "update":
+                UpdateData(Guid.Parse(args[1]), args[2]);
+                break;
+
+            case "delete":
+                DeleteData(Guid.Parse(args[1]));
+                break;
+
+            case "mark-in-progress":
+                UpdateStatus(Guid.Parse(args[1]), TaskStatusEnum.InProgress);
+                break;
+
+            case "mark-done":
+                UpdateStatus(Guid.Parse(args[1]), TaskStatusEnum.Done);
+                break;
+
+            case "mark-todo":
+                UpdateStatus(Guid.Parse(args[1]), TaskStatusEnum.Todo);
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+//Task Structure
+public class TaskCli
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+    public TaskStatusEnum Status { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+}
+
+public enum TaskStatusEnum
+{
+    Todo,
+    InProgress,
+    Done
+}
